af403271fcdf8d95485a4359e191136d6bf5503b
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2023-11-07 19:35:37 -0300

    feat: dwellers and build delete command

---
+++ src/commands/dwellers/actions/delete.ts
@@ -0,0 +1,22 @@
+import { ChatInputCommandInteraction } from 'discord.js';
+import { DwellerServices } from 'src/services/dweller.service';
+
+export const DeleteDweller = async (
+  interaction: ChatInputCommandInteraction,
+) => {
+  await interaction.deferReply({ ephemeral: false });
+  try {
+    const id = interaction.options.getString('dweller');
+
+    await DwellerServices.Delete(id!);
+
+    return await interaction.editReply({
+      content: 'Dweller succesfully banned!',
+    });
+  } catch (err) {
+    console.error(err);
+    await interaction.editReply({
+      content: "Whoa, seems to be a powerfull dweller. Couldn't be banned",
+    });
+  }
+};
